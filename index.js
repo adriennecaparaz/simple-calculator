27268bf580859cff060d1db706e6513dfd419438
Adrienne Caparaz <adcapz@gmail.com>
2022-09-29 03:37:56 -0700

    implemented display update

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let result = 0;
+const OPERATORS = ['clr', '%', 'neg', '/', '+', '-', '*', 'back', '='];
+
 const add = function(a, b) {
     return a + b;
 };
@@ -25,4 +28,18 @@ const operate = function(operator, a, b) {
         case '/':
             return divide(a, b);
     }
-}
+};
+
+const handleInput = function(e) {
+    const value = e.target.dataset['value'];
+    console.log(!OPERATORS.includes(value));
+    if (!OPERATORS.includes(value)) {
+        equation.textContent += value;
+    }
+};
+
+const equation = document.querySelector('.equation');
+const buttons = document.querySelectorAll('button');
+buttons.forEach(btn => {
+    btn.addEventListener('click', handleInput);
+});
